d2552203167ee67522653bc638377810a3f6ef58
Johni Cassere <johnicassere@hotmail.com>
2022-10-16 12:43:24 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// mongodb+srv://admin:admin@apicluster.kssalqy.mongodb.net/?retryWrites=true&w=majority
+require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const personRoutes = require("./routes/personRoutes");
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
+app.use("/person", personRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ message: "oi express" });
+});
+
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
+
+mongoose
+  .connect(
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.kssalqy.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("Conectado ao Banco");
+    app.listen(3001);
+  })
+  .catch((err) => console.log(err));
